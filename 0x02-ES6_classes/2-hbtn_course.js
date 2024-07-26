2b927130a3d2cb065e7ccbc9db6958425dc33478
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-07-26 02:13:50 +0100

    major correction using centralized validation (setters)

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -10,11 +10,10 @@ class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = name;
   }
 
   get length() {
@@ -22,11 +21,10 @@ class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
   get students() {
@@ -34,9 +32,10 @@ class HolbertonCourse {
   }
 
   set students(students) {
-    if (Array.isArray(students) && students.every((item) => typeof item === 'string')) {
-      this._students = students;
+    if (!Array.isArray(students) || !students.every(item => typeof item === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
+    this._students = students;
   }
 }
 
