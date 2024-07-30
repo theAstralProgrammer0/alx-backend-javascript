fdcd1cf2869ba1459c0e64f00beef1dae0b70bb1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-07-30 23:51:27 +0100

    Added lint config file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,15 @@
 class Students {
-  constructor (id, firstName, location) {
+  constructor(id, firstName, location) {
     this.id = id;
     this.firstName = firstName;
     this.location = location;
   }
 }
 
-function getListStudents () {
+function getListStudents() {
   const s1 = new Students(1, 'Guillame', 'San Francisco');
   const s2 = new Students(2, 'James', 'Columbia');
-  const s3 = new Students(3, 'Serena', 'San Francisco');
+  const s3 = new Students(5, 'Serena', 'San Francisco');
   return [s1, s2, s3];
 }
 
