bf1ec1f32f382aac1b8fe1f1684040f2b4e13a8c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-07-30 23:30:48 +0100

    Added task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,16 @@
+class Students {
+  constructor (id, firstName, location) {
+    this.id = id;
+    this.firstName = firstName;
+    this.location = location;
+  }
+}
+
+function getListStudents () {
+  const s1 = new Students(1, 'Guillame', 'San Francisco');
+  const s2 = new Students(2, 'James', 'Columbia');
+  const s3 = new Students(3, 'Serena', 'San Francisco');
+  return [s1, s2, s3];
+}
+
+export default getListStudents;
