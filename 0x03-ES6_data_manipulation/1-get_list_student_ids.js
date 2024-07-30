cdfe4710d751c498d3f7d62392d20a4ee06da3fd
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-07-31 00:47:34 +0100

    Removed id param from map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,12 @@
+module.exports = function getListStudentsIds(objs) {
+  if (!Array.isArray(objs)) return [];
+  return objs.map((obj) => obj.id);
+};
+
+/*
 function getListStudentsIds(objs) {
-  if (!Array.isArray(objs)) return []
+  if (!Array.isArray(objs)) return [];
   const [s1, s2, s3] = objs;
   return [s1.id, s2.id, s3.id];
 }
-
-module.exports = function getListStudentsIds(objs) {
-  if (!Array.isArray(objs)) return []
-  return objs.map((obj, id) => obj.id);
-}
+*/
