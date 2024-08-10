5962449655e146ee2b29081e4e0a315016bad910
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-08-10 06:09:25 +0100

    task 3 correct

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,3 @@
-const getStudentIdsSum = (students) => {
-  const ids = students.map((student) => student.id);
-  console.log(ids)
-  const sum = ids.reduce((acc,curr) => {acc+curr}, 0);
-  return sum
-}; 
-
-export default getStudentIdsSum;
+export default function getStudentIdsSum(list) {
+  return list.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+}
