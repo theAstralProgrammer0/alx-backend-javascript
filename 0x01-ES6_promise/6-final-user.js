fad6f4dba1fdc4f44520559bd01d9251f9ff0a32
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-04-17 20:44:24 +0100

    Added task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => (
+      results.map((e) => (
+        {
+          status: e.status,
+          value: e.value === 'fulfilled' ? e.value : e.reason,
+        }))));
 }
