42fa53f2debb5d3de67c740b1a755420cf1aae0e
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-04-23 23:08:13 +0100

    Added task 1 files

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,5 @@
+export default function getListStudentsIds(listOfStudents) {
+  if (listOfStudents instanceof Array) { return listOfStudents.map((student) => student.id) };
+  return [];
+  //return listOfStudents;
+}
